0f16314d06ad1fcd8efe3b8599dc52bb4287590b
Kevin Miller <kevin@driftabout.com>
2018-03-13 18:28:13 -0700

    tweaking radix sort

--- lab-kevin/lib/sort-algorithms.js
+++ lab-kevin/lib/sort-algorithms.js
@@ -2,7 +2,7 @@
 
 const sort_algorithms = module.exports = {};
 
-sort_algorithms.radixSort = itemArray => {
+sort_algorithms.radixSort2 = itemArray => {
   if(itemArray === null) return null;
   if (!itemArray || !Array.isArray(itemArray)) throw new Error('Type error: expecting array');
   if (itemArray.length < 2) return itemArray;
@@ -19,7 +19,7 @@ sort_algorithms.radixSort = itemArray => {
       if (!digitArray[digit] ) digitArray[digit] = [];
       digitArray[digit].push(itemArray[i]);
     }
-    
+
     if (digitArray.length === 1 && digitArray[0]) sorted = true;
 
     if (sorted) break;
@@ -30,6 +30,42 @@ sort_algorithms.radixSort = itemArray => {
     base = base * 10;
   }
 
+  return sortedArray;
+};
+
+
+sort_algorithms.radixSort = itemArray => {
+  if(itemArray === null) return null;
+  if (!itemArray || !Array.isArray(itemArray)) throw new Error('Type error: expecting array');
+  if (itemArray.length < 2) return itemArray;
+
+  let digitArray;
+  let sortedArray = itemArray;
+  let sorted = false;
+  let base = 1;
+
+  while (!sorted){
+    digitArray = [];
+    for(let i in sortedArray){
+      let digit = Math.floor((sortedArray[i] / base) % 10);
+      if (!digitArray[digit] ) digitArray[digit] = [];
+      digitArray[digit].push(sortedArray[i]);
+    }
+
+    if (digitArray.length === 1 && digitArray[0]) sorted = true;
+
+    if (sorted) break;
+    sortedArray = [];
+    
+    for(let i in digitArray){
+      sortedArray.push(...digitArray[i]);
+      // for(let j in digitArray[i]) {
+      //   sortedArray.push(digitArray[i][j]);
+      // }
+      console.log(base, sortedArray);
+    }
+    base = base * 10;
+  }
   return sortedArray;
 };
 
