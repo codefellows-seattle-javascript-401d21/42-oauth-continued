8a0cf162eedbc225d00860f6052b0333b356256f
Melanie Downing <sayanything830@gmail.com>
2018-03-13 17:22:25 -0700

    finished readme

--- lab-melanie/index.js
+++ lab-melanie/index.js
@@ -1,20 +1,15 @@
 'use strict';
 
-const mergeSort =  require('./lib/mergesort');
-const quicksort = require('./lib/quicksort');
+const mergeSort =  require('./lib/mergeSort');
+const quicksort = require('./lib/quickSort');
 
 let numericArray = [8, 7, 5, 4, 3, 6, 1, 0, 9, 15];
-let arrTwo = 'a';
 
 // let sortedArray = mergeSort(numericArray);
 
 // console.log(numericArray ,sortedArray);
+console.log('merge sorted', mergeSort(numericArray));
 
 quicksort(numericArray);
-quicksort(arrTwo);
+
 console.log('quick sorted:', numericArray);
-console.log('null array:', arrTwo);
-
-
-
-// console.log(mergeSort(numericArray));
