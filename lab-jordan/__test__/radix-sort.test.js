3900a32ec48d374e4257f2143d329f798b8524cd
Jordan Van Ness <jordanwvn@gmail.com>
2018-03-13 20:57:00 -0700

    minor reformatting, expanded testing. Now using testing functions to create random arrays of n items, and checking if an array is sorted. These are used to ensure that the sort works with 0, 1, 10, 100, and 1000 items.

--- lab-jordan/__test__/radix-sort.test.js
+++ lab-jordan/__test__/radix-sort.test.js
@@ -1,12 +1,37 @@
 'use strict';
 
-const sort = require('../lib/radix-sort.js');
+const radix = require('../lib/radix-sort.js');
 
-let test = [15, 43, 98, 100, 2, 73];
-let expected = [2, 15, 43, 73, 98, 100];
+let randomArray = n => {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    let number = Math.floor(Math.random() * (100 - 1) + 1);
+    result.push(number)
+  }
+  return result;
+}
+
+let sorted = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] > arr[i]) return false;
+  }
+  return true;
+}
 
 describe('radix sort', () => {
-  it('should sort the numbers correctly', () => {
-    expect(sort(test)).toEqual(expected);
+  it('empty array', () => {
+    expect(sorted(radix(randomArray(0)))).toEqual(true);
+  })
+  it('single element array', () => {
+    expect(sorted(radix(randomArray(1)))).toEqual(true);
+  })
+  it('small array', () => {
+    expect(sorted(radix(randomArray(10)))).toEqual(true);
+  })
+  it('medium array', () => {
+    expect(sorted(radix(randomArray(100)))).toEqual(true);
+  })
+  it('large array', () => {
+    expect(sorted(radix(randomArray(1000)))).toEqual(true);
   })
 })
--- lab-jordan/lib/radix-sort.js
+++ lab-jordan/lib/radix-sort.js
@@ -1,22 +1,21 @@
+'use strict';
+
 const radix_sort = module.exports = (arr, base=10) => {
   let maxVal = Math.max(...arr);
 
-  let iteration = 0;
-
-  while (base ** iteration <= maxVal) {
-    arr = buckets_to_list(list_to_buckets(arr, base, iteration));
-    iteration++;
+  for (let exponent = 0; (base ** exponent) <= maxVal; exponent++) {
+    arr = buckets_to_list(list_to_buckets(arr, base, exponent));
   }
   return arr;
 }
 
-const list_to_buckets = (arr, base, iteration) => {
+const list_to_buckets = (arr, base, exponent) => {
   let buckets = new Map();
 
   for (let i = 0; i < base; buckets.set(i, []), i++);
 
   for (let i = 0; i < arr.length; i++) {
-    let digit = Math.floor(arr[i] / (base ** iteration)) % base;
+    let digit = ~~(arr[i] / (base ** exponent)) % base;
     buckets.get(digit).push(arr[i]);
   }
   return buckets;
@@ -25,8 +24,7 @@ const list_to_buckets = (arr, base, iteration) => {
 const buckets_to_list = (buckets) => {
   let numbers = [];
 
-  for (let value of buckets.values()) {
-    numbers.push(...value)
-  }
+  for (let value of buckets.values()) numbers.push(...value);
+
   return numbers;
 }
