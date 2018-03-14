aa0fd64471e7d6661b84f0e0b45411615817d174
billodell <billodell5@gmail.com>
2018-03-13 20:45:53 -0700

    readme

--- lab-bill/src/mergesort.js
+++ lab-bill/src/mergesort.js
@@ -3,39 +3,35 @@
 const mergeSort = items => {
   let output = [];
   if(items.length < 2)
-      return items;
+    return items;
 
   if(items.length === 2){
-    // Vinicio - Extra memory!
     return items[0] < items[1] ? items : items.reverse();
   }
 
   const middlePoint = Math.floor(items.length / 2);
 
-  //----------------------------------------------------------------------
-  // Vinicio - is this the most optimal use of memory?
+
   const leftSide = mergeSort(items.slice(0,middlePoint));
   const rightSide = mergeSort(items.slice(middlePoint));
-  //----------------------------------------------------------------------
+
 
   while(leftSide.length || rightSide.length){
-    //--------------------------------------------------------------------
-    // Vinicio - Do we have all the elements on the left?
+
     if(leftSide.length && !rightSide.length){
-      output = output.concat(leftSide); // Vinicio - memory?
+      output = output.concat(leftSide); 
       break;
     }
-    // Vinicio - Do we have all the elements on the right?
+
     if(!leftSide.length && rightSide.length){
-      output = output.concat(rightSide); // Vinicio - memory?
+      output = output.concat(rightSide); 
       break;
     }
-    //--------------------------------------------------------------------
-    // Vinicio - We have elements on both sides
+
     if(leftSide[0] <= rightSide[0])
-      output.push(leftSide.shift()); // Vinicio - memory?
+      output.push(leftSide.shift()); 
     else
-      output.push(rightSide.shift()); // Vinicio - memory
+      output.push(rightSide.shift()); 
   }
   return output;
 };
