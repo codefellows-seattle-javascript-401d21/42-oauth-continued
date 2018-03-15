8f66afc84c0e2a1efa081cbf7be3a7424cfa13bd
strawbee <yuejiao.hou@gmail.com>
2018-03-15 01:24:36 -0700

    readme

--- lab-joy/index.js
+++ lab-joy/index.js
@@ -1,4 +1,3 @@
 'use strict';
-/* I constructed a heap data structure and a function that sorts an array of numbers using heap sort. Refer to lib/heap.js and lib/heapsort.js for code. */
 let Heap = require('./lib/heap'); // eslint-disable-line
 let heapsort = require('./lib/heapsort'); // eslint-disable-line
