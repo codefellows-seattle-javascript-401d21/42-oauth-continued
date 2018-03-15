40353f2223611b65a4826755f4afc25c45be7470
strawbee <yuejiao.hou@gmail.com>
2018-03-15 01:49:56 -0700

    modified tests

--- lab-joy/test/heapsort.test.js
+++ lab-joy/test/heapsort.test.js
@@ -4,10 +4,14 @@ let heapsort = require('../lib/heapsort');
 require('jest');
 
 describe('#heapsort function', () => {
-  const randomIntegers = n => Array.from({ length: n }, () => Math.floor(Math.random() * 10000));
+  const randomIntegers = n => Array.from({length: n}, () => ~~(Math.random() * 10000));
 
   it('should return an error for an empty array input', () => {
     expect(heapsort([])).toBe('Input must be a non-empty array.');
+  });
+
+  it('should return an error if input array elements are not numbers', () => {
+    expect(heapsort(['str', 1, true])).toBe('Elements must be numbers.');
   });
 
   it('should return the same array for a single element array input', () => {
@@ -29,6 +33,12 @@ describe('#heapsort function', () => {
   it('should correctly sort a large array', () => {
     let arr = randomIntegers(5000);
     let sorted = arr.sort();
+    expect(heapsort(arr)).toMatchObject(sorted);
+  });
+
+  it('should correctly sort non-integers and negative numbers', () => {
+    let arr = [0, 1.5, 1.2, 1.79, -3, -3.2];
+    let sorted = [-3.2, -3, 0, 1.2, 1.5, 1.79];
     expect(heapsort(arr)).toMatchObject(sorted);
   });
 });
